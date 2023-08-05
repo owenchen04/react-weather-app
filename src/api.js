f66b66aa4191ee2d64b59f38813779e77df260e4
Owen Chen <owenchen502@gmail.com>
2023-08-05 04:43:06 -0400

    Update api.js
    use environment secrets in api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': '0f9343d6bamshc416300bf273e59p17ed8fjsn3a15ed32b1f6',
+    'X-RapidAPI-Key': process.env.GEO_API_KEY,
     'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
   }
 };
@@ -9,4 +9,4 @@ export const geoApiOptions = {
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "1bf51be45b459c5d28328b7f334c1e24";
+export const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
